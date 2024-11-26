e3c5ec833a322956491a0ad22dc8f7d287ad43c8
Ben-Foxman <ben.foxman@yale.edu>
2024-11-26 12:51:22 -0500

    new render yaml

--- frontend-interface/next.config.mjs
+++ frontend-interface/next.config.mjs
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'standalone',
-};
+    images: {
+        unoptimized: true,
+    },
+    serverOptions: {
+        port: parseInt(process.env.PORT || '10000', 10)
+    }
+}
 
-export default nextConfig;
+module.exports = nextConfig
