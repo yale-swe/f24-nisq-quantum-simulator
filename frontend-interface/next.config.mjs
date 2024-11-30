6acd84c9d454b681fd1665dd466d63060810ae46
Ben-Foxman <ben.foxman@yale.edu>
2024-11-30 13:27:53 -0500

    fixing warnings for deployment

--- frontend-interface/next.config.mjs
+++ frontend-interface/next.config.mjs
@@ -1,14 +1,9 @@
-// next.config.mjs
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'standalone',
     images: {
         unoptimized: true,
-    },
-    experimental: {
-        serverActions: true,
     }
 }
 
-// Use export default instead of module.exports
 export default nextConfig;
